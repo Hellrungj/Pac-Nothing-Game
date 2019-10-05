2b67991ba0ab77f24ca66ba72b958399c8df1144
Hellrungj <johnjameshellrung@gmail.com>
2019-10-04 23:31:54 -0500

    Add player and enemy movement

---
+++ Nothing Game/Assets/Scripts/EnemyMovment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovment : MonoBehaviour
+{
+    public Transform Player;
+    public float speed = 5.0f;  //acceleration
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+//      
+        transform.LookAt(Player);
+
+        //if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        //{
+        transform.position += transform.forward * speed * Time.deltaTime;
+        //if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+        //{
+        //Here Call any function U want Like Shoot at here or something
+        //}
+
+        //}
+    }
+}
