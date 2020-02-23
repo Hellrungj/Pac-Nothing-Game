a21244f79ee5ecf459710426f8d8233ffef80c8c
kobiwall <kobi.wall@gmail.com>
2020-02-23 14:24:48 -0600

    Synched up scenes

---
+++ Labyrinth/Assets/PLayer_Collision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PLayer_Collision : MonoBehaviour
+{
+    public Player_Movement movement;
+    private void OnCollisionEnter(Collision collisioninfo)
+    {
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
